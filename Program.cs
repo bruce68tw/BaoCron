ee8a676d7edb9c9413e622af53de5a232d91410e
Bruce Chen <bruce68tw@gmail.com>
2021-11-23 19:45:43 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using BaoCron.Services;
+using Base.Models;
+using Base.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace BaoCron
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //1.initial & load EasyRptConfig.json
+            IConfiguration configBuild = new ConfigurationBuilder()
+                .AddJsonFile("BaoCronConfig.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            //2.appSettings "FunConfig" section -> _Fun.Config
+            var config = new ConfigDto();
+            configBuild.GetSection("FunConfig").Bind(config);
+            _Fun.Config = config;
+
+            //3.setup our DI
+            var services = new ServiceCollection();
+
+            //4.base user info for base component
+            services.AddSingleton<IBaseUserService, BaseUserService>();
+
+            //5.ado.net for mssql
+            services.AddTransient<DbConnection, SqlConnection>();
+            services.AddTransient<DbCommand, SqlCommand>();
+
+            //7.redis
+            //services.AddStackExchangeRedisCache(opts => { opts.Configuration = config.Redis; });
+
+            //6.initial _Fun by mssql
+            IServiceProvider diBox = services.BuildServiceProvider();
+            _Fun.Init(false, diBox);
+
+            //7.run main 
+            await new MyService().RunAsync();
+        }
+    }
+}
---
+++ Services/MyService.cs
@@ -0,0 +1,59 @@
+﻿using Base.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace BaoCron.Services
+{
+    public class MyService
+    {
+        public async Task RunAsync()
+        {
+            const string preLog = "BaoCron: ";
+            await _Log.InfoAsync(preLog + "Start.");
+
+            #region 1.清除 Redis Cache
+            var info = "";
+            _Redis.DeleteAll();
+            #endregion
+
+            #region 2.遊戲上下架
+            await using (var db = new Db())
+            {
+                //下架
+                var today = DateTime.Today;
+                var tmr = today.AddDays(1);
+                var sql = $@"
+update dbo.Bao
+    set LaunchStatus=3
+where LaunchStatus=2
+and EndTime < getdate()
+and Status=1
+";
+                await db.ExecSqlAsync(sql);
+
+                //上架
+                sql = $@"
+update dbo.Bao
+    set LaunchStatus=2
+where LaunchStatus=1
+and StartTime < getdate()
+and Status=1
+";
+                await db.ExecSqlAsync(sql);
+
+            }
+            #endregion
+
+        #region close db & log
+        lab_exit:
+            //if (db != null)
+            //    await db.DisposeAsync();
+            if (info != "")
+                await _Log.InfoAsync(preLog + info);
+
+            await _Log.InfoAsync(preLog + "End.");
+            #endregion
+        }
+
+    }//class
+}
